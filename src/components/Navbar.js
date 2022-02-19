9e356a1a5fb35c0e08d954d810082d2bd0d914f7
Alexander Brandon <alexlbrandon@gmail.com>
2022-02-19 16:32:28 -0500

    initial commit: created react application and installed tailwind css

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-10 bg-emerald-500 mb-3">
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </nav>
+    </>
+  );
+}
